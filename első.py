b039461b99d60339080b200855b39efd00d67ac6
Szantai Gabor <szantai.gabor.19f@szlgbp.hu>
2022-01-24 11:03:40 +0100

    alső

--- első.py
+++ első.py
@@ -1,6 +1,12 @@
 # 1. Van-e a sorozatban 100-zal osztható szám?
 
 def egy():
-bool van_e = False
-
-    while
+    van_e= False
+    i = 0
+    while not van_e and i<len(lista):
+        
+        if(lista[i]==100):
+            van_e=True
+        i+=1
+    
+    print(van_e)
