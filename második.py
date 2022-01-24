bfc48f52a3e89d24da8cc4bde099e42e1d586c1a
Szantai Gabor <szantai.gabor.19f@szlgbp.hu>
2022-01-24 11:11:26 +0100

    második

---
+++ második.py
@@ -0,0 +1,12 @@
+def kettő (lista):
+    index = 0
+    i = 0
+    while i<len(lista):
+        
+        if lista[i]%7==0:
+            index=i
+        
+        i+=1
+            
+
+    print (kettő)        
