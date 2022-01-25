a47c8c23327fdff6db88545c84251a95aea4d6eb
Niki Jávor <nikolettjavor@gmail.com>
2022-01-25 10:20:50 +0100

    i

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from első import egy
 from második import kettő
-#from harmadik import három
+from harmadik import három
 #from negyedik import négy
 #from ötödik import öt
 from hatodik import hat
@@ -14,17 +14,18 @@ with open("input.txt","r",encoding="utf8") as f:
     for sor in f:
         lista.append(int(sor))
 
-"""
 print("1. Van-e a sorozatban 100-zal osztható szám?")
 egy(lista)
 print("2. Írjuk ki az utolsó 7-tel osztható szám indexét!")
 kettő(lista)
+"""
 print("3. Írjuk ki az első 19-cel osztható szám indexét!")
 három(lista)
 print("4. Mennyi a sorozatban található számok átlagának a négyzete?")
 négy(lista)
 print("5. Igaz-e, hogy minden szám nagyobb, mint 10?")
 öt(lista)
+"""
 print("6. Hány 9-cel osztható szám található a sorozatban?")
 hat(lista)
 print("7. Írjuk ki a sorozatban található 15-tel osztható számok felét!")
@@ -35,4 +36,3 @@ print("9. Van-e a sorozatban olyan negatív szám, amelyet pozitív követ?")
 kilenc(lista)
 print("10. Mennyi a sorozatban található legkisebb szám fele?")
 tíz(lista)
-"""
