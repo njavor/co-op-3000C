c9a4d1674dedd558d4257b25b7a69bb06c2fe641
Szantai Gabor <szantai.gabor.19f@szlgbp.hu>
2022-01-25 10:22:35 +0100

    harmadik

--- harmadik.py
+++ harmadik.py
@@ -1,2 +1,12 @@
 def három(lista):
-    
+    tizenkilenc_megvan=False
+    i=0
+    index=0
+    while not tizenkilenc_megvan and i<len(lista):
+        if lista[i]%19==0:
+            index =i
+            tizenkilenc_megvan=True
+
+        i+=1
+
+    print(index)    
